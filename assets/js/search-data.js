a2da297bb0e60cad9397bf120415ea0cd3f2fc12
smcastellanosp <smcastellanosp@gmail.com>
2025-08-19 13:51:09 +0000

    Deploying to gh-pages from @ smcastellanosp/smcastellanosp.github.io@d403b8e09e1be3611ef6e97ec3b2b940fcc472f2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -21,16 +21,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-i-am-delighted-to-share-that-i-will-be-taking-part-in-ijcai-2025-in-montreal-august-16-22-2025",
+          title: 'I am delighted to share that I will be taking part in IJCAI...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
